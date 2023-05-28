c7482bcc23dbf8102e5f7c3ed1a6e3beae60feb3
Roy Ivy III <rivy.dev@gmail.com>
2023-05-28 01:50:37 -0500

    wip ~ path canon/normal experiments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod fs;
+use std::path::Path;
+
+fn main() -> std::process::ExitCode {
+    let args: Vec<_> = wild::args_os().collect();
+    println!("args {:?}", args);
+
+    args.iter().for_each(|arg| {
+        let std_canon_arg = std::fs::canonicalize(arg);
+        let dunce_canon_arg = dunce::canonicalize(arg);
+        let canon_arg =
+            fs::canonicalize(arg, fs::MissingHandling::Normal, fs::ResolveMode::Physical);
+        let normal_arg = fs::normalize_path(Path::new(arg));
+        println!(
+            "arg {:?} => std_canon {:?}, dunce_canon {:?}, uu_canon {:?}, uu_normal {:?}",
+            arg, std_canon_arg, dunce_canon_arg, canon_arg, normal_arg
+        );
+    });
+
+    std::process::ExitCode::SUCCESS
 }
